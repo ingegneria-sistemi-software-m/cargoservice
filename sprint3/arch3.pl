376dda28cde63ac6a321974ce81a81921c05de23
kevin <kevinkoltraka1011@gmail.com>
2025-07-31 18:00:22 +0200

    aggiunto volume dentro ad arch3-rpi.yaml

--- sprint3/arch3.pl
+++ sprint3/arch3.pl
@@ -51,7 +51,7 @@ request( getenvmap, getenvmap(X) ).
 reply( envmap, envmap(MAP) ).  %%for getenvmap
 %====================================================================================
 context(ctx_cargoservice, "localhost",  "TCP", "8000").
-context(ctx_iodevices, "192.168.28.129",  "TCP", "8001").
+context(ctx_iodevices, "192.168.1.158",  "TCP", "8001").
 context(ctx_basicrobot, "my-basicrobot",  "TCP", "8020").
 context(ctx_productservice, "cargoserviceqak",  "TCP", "8111").
  qactor( basicrobot, ctx_basicrobot, "external").
